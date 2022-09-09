044c2ae103d13f2f91a1a02b3114c040f8065e94
ojnode <ojinnakabu95@gmail.com>
2022-09-09 06:27:49 -0700

    the last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - program determing if number is greater than 5, less than 6 or 0
+*
+* Return: Always 0 (succes)
+*
+*/
+
+int main(void)
+{
+int n, lastDigit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastDigit = n % 10;
+if (lastDigit > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+else if (lastDigit < 6 && lastDigit != 0)
+printf("Last digit of %d is %d and is than 6 and is not 0", n, lastDigit);
+else
+printf("Last digit of %d is %d and is 0", n, lastDigit);
+
+return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,6 @@
 C programming on alx Variables, if, else, while
 
 0-positive_or_negative.c : a program to print whether the number stored in the variable n is positive or negative.
+
+1-last_digit.c: This program will assign a random number to the variable n each time it is executed and will determine if the last digit of the number is greater than 5, less than 6 or not equal to 0 or equal to 0, and print the corresponding result.
+
