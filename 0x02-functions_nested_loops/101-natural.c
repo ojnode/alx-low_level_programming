6d4051a77ec422da26ba392526f3029db80fafcc
ojnode <ojinnakabu95@gmail.com>
2022-09-14 07:04:00 -0700

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - lists all multiples of 3 and 5 below 1024
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
