df05869620827612da93ea6fcdb7b826ec586ab0
ojnode <ojinnakabu95@gmail.com>
2022-09-15 08:08:53 -0700

    draw a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * @n: is the number of times _ printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n < 0)
+		_putchar('\n');
+	else
+	{
+	for (i = 0; i < n; i++)
+	_putchar('_');
+	_putchar('\n');
+	}
+}
+
