6923e74253377bb6bc8a805da6666da8e09ed4f2
ojnode <ojinnakabu95@gmail.com>
2022-09-22 11:39:00 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
-	while (dest[dest[index++])
+	while (dest[index++])
 		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
