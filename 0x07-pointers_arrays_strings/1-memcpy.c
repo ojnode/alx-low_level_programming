0775f9a6438166b42c990ad3beb1d617f6c95c91
ojnode <ojinnakabu95@gmail.com>
2022-09-26 11:44:04 -0700

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - memset function
+ * @dest: content is to be copied
+ * @src: source of datea to be copied
+ * @n: bytes of the memory
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
+
