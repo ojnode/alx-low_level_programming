e301c38a79954a9e189014850793d023ffcdea03
ojnode <ojinnakabu95@gmail.com>
2022-10-06 19:19:00 +0100

    fixed for betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,6 +12,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
 	unsigned int size1 = 0, size2 = 0, i;
+
 	if (s1 == NULL)
 		s1 = "";
 
@@ -37,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[i] = s1[i];
 	}
 
-	for (; i <(size1 + n); i++)
+	for (; i < (size1 + n); i++)
 	{
 		p[i] = s2[i - size1];
 	}
