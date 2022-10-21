ba7d9225a6caa8b7c127ca14d639bcb13f628b86
ojnode <ojinnakabu95@gmail.com>
2022-10-21 10:01:50 +0100

    edit list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
+
+/**
+ * struct list_s - singly list
+ * @str: string from malloc
+ * @len: string length
+ * @next: next mode
+ */
 
 typedef struct list_s
 {
@@ -14,5 +22,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif
