a15742d686064120ce090ec94be3d914ab72d739
ojnode <ojinnakabu95@gmail.com>
2022-10-21 10:13:39 +0100

    add note

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * _getlen - get length of string
+ * @str: steing to find length
+ *
+ * Return: length in int type
+ */
+unisigned int _getlen(char *str)
+{
+	unsigned int i;
+
+	while (*(str + i))
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node_end - adds a new node to end of link list
+ * @head: pointer of pointer to linked ist
+ * @str: string to be add to linked list
+ *
+ * Return: list_t
+ */
+list_t *add_note_end(list_t **head, const char *str)
+{
+	list_t *endnew, *temp;
+
+	if (str == NULL)
+		return (NULL);
+	endnew = malloc(sizeof(list_t));
+	if (endnew == NULL)
+		return (NULL);
+	endnew->str = strdup(str);
+
+	if (endnew->str == NULL)
+	{
+		free(endnew);
+		return (NULL);
+	}
+	endnew->len = _getlen(endnew->str);
+	endnew->next = NULL;
+	if (*head == NULL)
+	{
+		*head = endnew;
+		return (endnew);
+	}
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = endnew;
+	return (endnew);
+}
