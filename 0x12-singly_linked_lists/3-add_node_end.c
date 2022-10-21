3598edfb963cfc2a18d10c397894bc812c07bcdc
ojnode <ojinnakabu95@gmail.com>
2022-10-21 17:29:23 +0100

    stringly list3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,29 +1,13 @@
 #include "lists.h"
 
-/**
- * _getlen - get length of string
- * @str: steing to find length
- *
- * Return: length in int type
- */
-unisigned int _getlen(char *str)
-{
-	unsigned int i;
-
-	while (*(str + i))
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
+/*
  * add_node_end - adds a new node to end of link list
  * @head: pointer of pointer to linked ist
  * @str: string to be add to linked list
  *
  * Return: list_t
  */
+
 list_t *add_note_end(list_t **head, const char *str)
 {
 	list_t *endnew, *temp;
