2458669fc19af365e538066cd48c156ff4d26836
ojnode <ojinnakabu95@gmail.com>
2022-10-21 17:35:39 +0100

    stringly list3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,30 +10,30 @@
 
 list_t *add_note_end(list_t **head, const char *str)
 {
-	list_t *endnew, *temp;
+	list_t *new, *temp;
+	size_t nchar;
 
-	if (str == NULL)
-		return (NULL);
-	endnew = malloc(sizeof(list_t));
-	if (endnew == NULL)
-		return (NULL);
-	endnew->str = strdup(str);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	return (NULL);
 
-	if (endnew->str == NULL)
-	{
-		free(endnew);
-		return (NULL);
-	}
-	endnew->len = _getlen(endnew->str);
-	endnew->next = NULL;
-	if (*head == NULL)
-	{
-		*head = endnew;
-		return (endnew);
-	}
+	new->str = strdup(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+	;
+	new ->len = nchar;
+	new->next = NULL;
 	temp = *head;
-	while (temp->next)
-		temp = temp->next;
-	temp->next = endnew;
-	return (endnew);
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+	while (temp->next != NULL)
+	temp = temp->next;
+	temp->next = new;
+	}
+	return (*head);
 }
