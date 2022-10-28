f486e1839d45f45d535735f7c3751afea8b81c6b
ojnode <ojinnakabu95@gmail.com>
2022-10-28 22:53:24 +0100

    fix binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,13 @@
-nclude "main.h"
+include "main.h"
 
 /**
- *  * binary_to_uint - converts a binary number to an
- *   * unsigned int.
- *    * @b: binary.
- *     *
- *      * Return: unsigned int.
- *       */
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
